4c510a0a552756e0b6604667a44a4df168460533
Gabrielraian <gabrielraian321@gmail.com>
2023-01-29 21:09:32 -0300

    projeto

---
+++ script.js
@@ -0,0 +1,7 @@
+const buttonEmail = document.getElementById('button-email')
+
+buttonEmail.addEventListener('click', alertSaveEmail)
+
+function alertSaveEmail() {
+  alert('email salvo com sucesso!')
+}
